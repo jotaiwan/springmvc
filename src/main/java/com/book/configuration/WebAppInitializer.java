faf2b751c3b5e50bf4da1a882bfafa845186417a
jotaiwan <jotaiwan@gmail.com>
2017-08-20 09:39:09 +1000

    chapter7.1

---
+++ src/main/java/com/book/configuration/WebAppInitializer.java
@@ -0,0 +1,37 @@
+package com.book.configuration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+/**
+ * Created by jotaiwan on 20/08/2017.
+ */
+public class WebAppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext container) {
+        // Create the 'root' Spring application context
+        AnnotationConfigWebApplicationContext rootContext =
+                new AnnotationConfigWebApplicationContext();
+        rootContext.register(RootConfig.class);
+
+        // Manage the lifecycle of the root application context
+        container.addListener(new ContextLoaderListener(rootContext));
+
+        // Create the dispatcher servlet's Spring application context
+        AnnotationConfigWebApplicationContext dispatcherContext =
+                new AnnotationConfigWebApplicationContext();
+        dispatcherContext.register(WebConfig.class);
+
+        // Register and map the dispatcher servlet
+        ServletRegistration.Dynamic dispatcher =
+                container.addServlet("dispatcher", new DispatcherServlet(dispatcherContext));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+}
--- src/main/java/com/book/configuration/WebInit.java
+++ src/main/java/com/book/configuration/WebInit.java
@@ -1,26 +1,28 @@
-package com.book.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
-/**
- * Created by jotaiwan on 29/07/2017.
- */
-@Configuration
-public class WebInit extends AbstractAnnotationConfigDispatcherServletInitializer {
-
-    @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[] { RootConfig.class };
-    }
-
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class<?>[] { WebConfig.class };
-    }
-
-    @Override
-    protected String[] getServletMappings() {
-        return new String[] {"/"};
-    }
-}
+//package com.book.configuration;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+//
+///**
+// * Created by jotaiwan on 29/07/2017.
+// *
+// * Note: replaced by WebAppInitializer, comment the code but keep as copy
+// */
+//@Configuration
+//public class WebInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+//
+//    @Override
+//    protected Class<?>[] getRootConfigClasses() {
+//        return new Class<?>[] { RootConfig.class };
+//    }
+//
+//    @Override
+//    protected Class<?>[] getServletConfigClasses() {
+//        return new Class<?>[] { WebConfig.class };
+//    }
+//
+//    @Override
+//    protected String[] getServletMappings() {
+//        return new String[] {"/"};
+//    }
+//}
